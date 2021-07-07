e321b6a3f1e740a15c532c55ee21813db7520be4
user <mbuguanatasha01@gmail.com>
2021-07-07 20:30:24 +0300

    defined models and migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid # Required for unique book instances
 
 # Create your models here.
 class Genre(models.Model):
@@ -22,6 +23,7 @@ class Book(models.Model):
     isbm = models.CharField('ISBM', max_length=13, unique=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    Language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
@@ -31,3 +33,52 @@ class Book(models.Model):
         return reverse('book-detail', arg=[str(self.id)])
 
 
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter the language of the book (e.g. Farsi, English etc)')
+
+    def __str__(self):
+        return self.name
