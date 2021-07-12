179afd2c02f5e9150f4ff7eef27374cbef942224
user <mbuguanatasha01@gmail.com>
2021-07-12 13:57:53 +0300

    add view for borrowed books, add borrower field to admin

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre, Language
+#from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+#@login_required
 def index(request):
      # Generate counts of some of the main objects
      num_books = Book.objects.all().count()
@@ -55,3 +57,13 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
