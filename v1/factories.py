3122b1afb651833530125a3ecaf43bedae27abc7
user <mbuguanatasha01@gmail.com>
2021-08-01 11:16:03 +0300

    chore: generate name using faker

--- v1/factories.py
+++ v1/factories.py
@@ -1,13 +1,24 @@
 import factory
 from faker import Factory
-from catalog.models import *
+from catalog.models import (
+    Book, 
+    Genre,
+    Author,
+    Language,
+    BookInstance,
+)
 
 faker=Factory.create()
-
-
 class GenreFactory(factory.django.DjangoModelFactory):
     
     class Meta:
         model = Genre
     
     name = "Adventure"
+
+class LanguageFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = Language
+
+    name = faker.name()
