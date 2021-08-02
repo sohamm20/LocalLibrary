625b7ffaf892c9bb539ebc0ff39a20f3d92193d9
user <mbuguanatasha01@gmail.com>
2021-08-02 09:33:42 +0300

    chore: correct Book model field name and migrate

--- catalog/models.py
+++ catalog/models.py
@@ -22,7 +22,7 @@ class Book(models.Model):
     isbm = models.CharField('ISBM', max_length=13, unique=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    Language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
